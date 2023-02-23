67e8787eaf01e0bdc2f160faf8af54d42688f2fd
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-23 10:17:10 +0600

    third bracket object notetion practice

---
+++ accessingObject.js
@@ -0,0 +1,13 @@
+const object={
+    name:'Alex',
+    work:'student',
+    gender:'male'
+}
+
+
+const work=object['work'];
+console.log(work);
+const firstName='name';
+const fName=object[firstName];
+console.log(fName);
+
