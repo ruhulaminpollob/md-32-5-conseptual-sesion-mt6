162785acdcbbb735d78502737ad7c5ec16fc734e
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-23 09:20:51 +0600

    practice ternary

--- ternary.js
+++ ternary.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+// if else conditions 
+
+const isStudent=false;
+let student='';
+
+// if (isStudent===true) {
+//     student='I am a good student';
+// }else{
+//     student='I am a trash cleaner';
+// }
+// console.log(student);
+
+
+
+// same as above 
+
+student=(isStudent===true) ? "I am a student" : "I am a trash cleaner";
+console.log(student);
